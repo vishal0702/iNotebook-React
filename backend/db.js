46ce845ef6d75f43a1e3c4810cda6fbb5e9b265c
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-09-08 17:28:19 +0530

    Enabled DB connection

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017/";
+
+const connectToMongo = async () => {
+  await mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch((err) => console.log(err));
+  //   console.log("Connected to MongoDB successfully");
+};
+
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Vishal!')
+})
+
+app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at http://127.0.0.1:${port}`)
+})
+
