56d2ddb6097e6d9fdfd47c2b5dadfe4b416d8d4c
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-09-12 12:16:56 +0530

    Stored data into db using Mongoose model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-
-router.get('/', (req, res) =>{
-    obj = {
-        name : "Vishal"
-    }
-
-    res.json(obj);
+//Create a User using: Post "/api/auth/", Doesn't require auth
+router.post('/', (req, res) =>{
+    const user = User(req.body);
+    user.save();
+    console.log(req.body);
+    res.json(req.body);
 })
 
 module.exports = router;
