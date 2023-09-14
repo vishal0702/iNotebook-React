f567dfb3b428885b7ec8bfda5e060c063a6d2808
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-09-14 18:02:02 +0530

    Hashed password using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
+const JWT_SECRET = "secretkey@password";
 //Create a User using: Post "/api/auth/createUser", Doesn't require auth
 router.post(
   "/createUser",
@@ -29,14 +32,23 @@ router.post(
           errors: "Sorry! A user already exists with the same email.",
         });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPassword = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPassword,
       });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
       // .then((user) => res.json(user)).catch(err => {console.log(err)
       // res.json({error : 'Duplicate email found! Please re-check.', message: err.message})})
-      res.json(user);
+      // res.json(user);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error occured");
