a0efbfe1e23b397a2dfe3c5a7c969285a0b2e080
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-09-14 17:00:04 +0530

    Enhanced create user API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-//Create a User using: Post "/api/auth/", Doesn't require auth
+//Create a User using: Post "/api/auth/createUser", Doesn't require auth
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Name must contain atleast 3 characters!").isLength({
       min: 3,
@@ -15,18 +15,32 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }).then((user) => res.json(user)).catch(err => {console.log(err)
-    res.json({error : 'Duplicate email found! Please re-check.', message: err.message})})
-    // res.json(req.body);
+
+    try {
+      //Check whether user with same email is already presend in DB
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({
+          errors: "Sorry! A user already exists with the same email.",
+        });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      // .then((user) => res.json(user)).catch(err => {console.log(err)
+      // res.json({error : 'Duplicate email found! Please re-check.', message: err.message})})
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 
